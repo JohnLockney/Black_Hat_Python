8ce5cde1fcb387904346900c155ed8ad1c8f8ab4
John Lockney <john.lockney@gmail.com>
2022-12-16 17:07:50 -0500

    fixe slf.ihl line

--- Chapter-3-Writing-a-Sniffer/sniffer_with_icmp.py
+++ Chapter-3-Writing-a-Sniffer/sniffer_with_icmp.py
@@ -16,7 +16,7 @@ class IP:
     def __init__(self, buff=None):
         header = struct.unpack('<BBHHHBBH4s4s', buff)
         self.ver = header[0] >> 4
-        self.ihl = header[0] >> 0xF
+        self.ihl = header[0] & 0xF
 
         self.tos = header[1]
         self.len = header[2]
